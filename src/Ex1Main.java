7f48a2f72fdf66dbbe57077fc785a452864a669a
Oshri-Zalman <Oshri.Zalman@msmail.ariel.ac.il>
2024-12-10 23:57:18 +0200

    update

--- src/Ex1Main.java
+++ src/Ex1Main.java
@@ -1,42 +1,66 @@
-import javax.swing.*;
+
 import java.util.Scanner;
-
-/**
- * Intro2CS, Ex1 - very basic "main template"
- * Make sure your implementation of this main performs as the Ex1Sol.jar solution implement all needed functions.
- *
- */
-
 
 public class Ex1Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String num1 = "", num2="", quit = "quit";
-        while (!num1.equals(quit) && !num2.equals(quit)) {
-            System.out.println();
-            System.out.println("Ex1 class solution:");
-            System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
-            num1 = sc.nextLine();
-            if (!num1.equals("quit")) {
-                boolean num1format = Ex1.isNumber(num1);
-                if (num1format) {
-                    System.out.println("its in the format good job!");
-                    System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
-                    num2 = sc.nextLine();
+     Scanner sc = new Scanner(System.in);
+      String num1 = "", num2 = "", quit = "quit";
 
-                    if (!num2.equals("quit")) {
-                        boolean num2format = Ex1.isNumber(num2);
-                        if (num2format) {
-                            System.out.println("its in the format good job!");
+      while (!num1.equals(quit) && !num2.equals(quit)) {
+         System.out.println();
+         System.out.println("Ex1 class solution:");
+         System.out.println("Enter a string as number#1 (or \"quit\" to end the program): ");
+         num1 = sc.nextLine();
+
+          if (!num1.equals(quit)) {
+           boolean isNum1Valid = Ex1.isNumber(num1);
+           int value1 = -1;
+
+             if (isNum1Valid) {
+               value1 = Ex1.number2int(num1);
+               System.out.println("num1= " + num1 + " is number: " + isNum1Valid + " , value: " + value1);
+               System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
+                num2 = sc.nextLine();
+
+                 if (!num2.equals(quit)) {
+                   boolean isNum2Valid = Ex1.isNumber(num2);
+                    int value2 = -1;
+                    if (isNum2Valid) {
+                      value2 = Ex1.number2int(num2);
+                      }
+                        System.out.println("num2= " + num2 + " is number: " + isNum2Valid + " , value: " + value2);
+
+                        if (!isNum2Valid) {
+                            System.out.println("ERR: num2 is in the wrong format! (" + num2 + ")");
                         } else {
-                            System.out.println("its isnt in the format try again!");
+                            System.out.println("Enter a base for output: (a number [2,16]): ");
+                            int base = sc.nextInt();
+                            sc.nextLine();
+
+                            if (base < 2 || base > 16) {
+                                System.out.println("ERR: wrong base, should be [2,16], got (" + base + ")");
+                            } else {
+                                int sum = value1 + value2;
+                                int product = value1 * value2;
+                                String sumInBase = Ex1.int2Number(sum, base);
+                                String productInBase = Ex1.int2Number(product, base);
+
+                                System.out.println(num1 + " + " + num2 + " = " + sumInBase);
+                                System.out.println(num1 + " * " + num2 + " = " + productInBase);
+
+                                String[] numbers = {num1, num2, sumInBase, productInBase};
+                                int maxIndex = Ex1.maxIndex(numbers);
+                                String maxNumber = numbers[maxIndex];
+                                System.out.println("Max number over [" + String.join(",", numbers) + "] is: " + maxNumber);
+                            }
                         }
                     }
                 } else {
-                    System.out.println("its isnt in the format try again!");
+                    System.out.println("ERR: num1 is in the wrong format! (" + num1 + ")");
                 }
             }
         }
-        System.out.println("quiting now...");
+        System.out.println("quitting now....");
     }
 }
+
